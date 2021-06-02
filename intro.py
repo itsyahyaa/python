4c5f25a16ba1473edc9953f0285d0798e8854042
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-02 22:37:34 +0300

    operation

--- intro.py
+++ intro.py
@@ -4,7 +4,7 @@ x= "yahya"
 print(a)
 print(7)
 print(x)
-
+# number
 b = 6
 c = 5 + 6
 d= a + b
@@ -13,3 +13,7 @@ print(c)
 print(d)
 
 print("hello kenya")
+
+# operation in number
+x1 = 6+6*4
+print(x1)
